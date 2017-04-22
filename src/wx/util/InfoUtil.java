956610c7e005709843a11515fe1b3415652bada1
Administrator <Administrator@WENJU-PC>
2017-04-22 23:05:04 +0800

    简化代码

--- src/wx/util/InfoUtil.java
+++ src/wx/util/InfoUtil.java
@@ -1,10 +1,8 @@
 package wx.util;
 
 import java.math.BigDecimal;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Random;
 
 public class InfoUtil {
 	
@@ -32,50 +30,4 @@ public class InfoUtil {
 		if(date==null) date=new Date();
 		return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(date);
 	}
-	 
-	 /**
-	  * 检查2个毫秒是否为同一天
-	  */
-	public static boolean isSameDay(long t1,long t2) {
-	SimpleDateFormat sdf1= new SimpleDateFormat("yyyyMMdd");
-	  String d1 =sdf1.format(t1);
-	  String d2 =sdf1.format(t2);
-	  return d1.equals(d2);
-	}
-	
-	/**
-	 * 获取今天0点的毫秒数
-	 */
-	public static long getTodayMillis() throws ParseException{
-		SimpleDateFormat sdf1= new SimpleDateFormat("yyyyMMdd");
-		SimpleDateFormat sdf2= new SimpleDateFormat("yyyyMMddHHmmss");
-		String today=sdf1.format(System.currentTimeMillis())+"000000";
-        return sdf2.parse(today).getTime();
-	}
-	
-	/**
-	 * 获取日期
-	 * yyyy-MM-dd HH:mm
-	 */
-	public static String getTime(long time){
-		SimpleDateFormat sdf3= new SimpleDateFormat("yyyy-MM-dd HH:mm");
-		return sdf3.format(time);
-	}
-	
-	/**
-	 * 生成随机数 不含4
-	 * @param count 随机数个数
-	 */
-	 public static int getRandom(int count){
-	        StringBuffer sb = new StringBuffer();
-	        String str = "12356789";
-	        Random r = new Random();
-	        for(int i=0;i<count;i++){
-	            int num = r.nextInt(str.length());
-	            sb.append(str.charAt(num));
-	            str = str.replace((str.charAt(num)+""), "");
-	        }
-	        return Integer.parseInt(sb.toString());
-	  }
-	 
 }
